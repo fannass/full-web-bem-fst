6af66a37f733fc8e3f22316f3cffc51ea95497ed
fannass <hamishmacallister41@gmail.com>
2026-04-22 23:02:17 +0700

    Validate organization update payload fields

--- backend-nest/src/modules/organization/dto/update-organization.dto.ts
+++ backend-nest/src/modules/organization/dto/update-organization.dto.ts
@@ -1,8 +1,29 @@
+import { IsEmail, IsObject, IsOptional, IsString, MaxLength } from 'class-validator';
+
 export class UpdateOrganizationDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
   name?: string;
+
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsOptional()
+  @IsString()
   address?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
   phone?: string;
+
+  @IsOptional()
+  @IsObject()
   social_media?: Record<string, string>;
 }
